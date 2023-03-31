2ed8b963b8184ef8f5ba02f7c0fcafc20213a71a
Mariana Costa <mariana.costa214@gmail.com>
2023-03-31 12:36:31 +0200

    chore: revert changes to paths

--- src/aux/fileHandler.js
+++ src/aux/fileHandler.js
@@ -1,19 +1,21 @@
-import udpatePageTitle from "./updatePageTitle.js";
+import udpatePageTitle from './updatePageTitle.js';
 
 let _currFile;
 
 const FileHandler = () => {
   const fnames = [
-    "carnationRevolution.json",
-    "formattedOgExample.json",
-    "littleRedRidingHood.json",
-    "t2s.json",
-    "capitolRiot.json",
+    'carnationRevolution.json',
+    'formattedOgExample.json',
+    'littleRedRidingHood.json',
+    't2s.json',
+    'capitolRiot.json',
   ];
 
   async function file(filename) {
     udpatePageTitle(filename);
-    const response = await fetch(`../assets/narratives/${filename}`);
+    const response = await fetch(
+      `/news-story-viz/assets/narratives/${filename}`
+    );
     _currFile = filename;
     const data = await response.json();
     data.filename = filename;
